7aab4c7925a55f92531a91d2324ef57307fe6a37
Minerobot23 <93414276+Minerobot23@users.noreply.github.com>
2023-12-10 00:37:20 -0500

    Initial Commit

---
+++ PlannedPotato/PlannedPotatoApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlannedPotatoApp.swift
+//  PlannedPotato
+//
+//  Created by Cristhian Garcia on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PlannedPotatoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
