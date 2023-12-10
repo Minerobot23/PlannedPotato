c3f744722aba34be981be8b7c2e6b1dce2487181
PlannedPotato <153413862+PlannedPotato@users.noreply.github.com>
2023-12-10 16:12:47 -0500

    Update PlannedPotatoApp.swift

--- PlannedPotato/PlannedPotatoApp.swift
+++ PlannedPotato/PlannedPotatoApp.swift
@@ -16,7 +16,7 @@ struct PlannedPotatoApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
             
-            #magdiel is getting carried
+            # screw yuy
                 
             }
         }
